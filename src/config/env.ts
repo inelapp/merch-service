0f3e0ca93c6e8574eea61fd56174fcb49608e6e3
Rodrigo Chumpitaz <chumpitazr130@gmail.com>
2024-11-16 00:19:39 -0500

    feat: change port to deploy

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || 3001;
+export const PORT = process.env.PORT || 3004;
 export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/express-mongo';
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret';
