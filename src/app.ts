e158eb7b431aae66564d88c0297cddac430495e7
Rodrigo Chumpitaz <chumpitazr130@gmail.com>
2024-12-03 00:58:30 -0500

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import userService from './services/user.service';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+	})
+);
 
 app.use('/api/v1', userService);
 
